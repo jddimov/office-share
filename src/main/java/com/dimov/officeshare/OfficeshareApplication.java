602a9648e44ae1433b9fdcd10d298787390652a2
DESKTOP-054F0V5\Jaro <jddimov@abv.bg>
2021-08-30 18:09:42 +0300

    Initial commit
    -- Add initial project setup

---
+++ src/main/java/com/dimov/officeshare/OfficeshareApplication.java
@@ -0,0 +1,13 @@
+package com.dimov.officeshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dimov/officeshare/OfficeshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimov.officeshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
